99ed78aeca58b716596c4534d2d732e128057d75
Xibooon <82497611+Xibooon@users.noreply.github.com>
2021-04-29 23:29:37 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -3,4 +3,4 @@ sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
 cd aytch
 chmod +x minergate-cli
-./minergate-cli -u watch998@outlook.com --xmr
+./minergate-cli -u penisspammail@gmail.com --xmr
